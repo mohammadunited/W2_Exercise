be220f5a9e075ca7d9a6746b2ec0af3acf13e6df
Mohammad Hadi <mhadi1@appdev.yearup.org>
2024-10-09 23:29:56 -0500

    4a assignment

---
+++ 4A_d atabase_insert.sq l.sql
@@ -0,0 +1,13 @@
+SELECT * FROM restaurantdata.result;
+
+Insert INTO result(Receipts,revenueNO,profitmade,paymentmethod)
+	Values('12234','10','2.5','card');
+
+Insert INTO result(Receipts,revenueNO,profitmade,paymentmethod)
+	Values('12255','8','1.5','card');
+
+Insert INTO result(Receipts,revenueNO,profitmade,paymentmethod)
+	Values('12654','95','15.5','cash');
+
+Insert INTO result(Receipts,revenueNO,profitmade,paymentmethod)
+	Values('12575','9','3.5','card');
